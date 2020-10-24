4a67082705f2e42710a8a366ba8732bf4e7719da
Бабышев Максим <cmdazaza@gmail.com>
2020-10-24 17:06:31 +0300

    Class unification + Extra details printed + values get from input

--- main.py
+++ main.py
@@ -2,48 +2,78 @@ from decimal import Decimal
 
 
 class Expression:
-    _str_repr = "{x} = {ans}"
+    _str_repr = "{x}"
+    _static_vars = dict()
+    _dynamic_vars = ['x']
+    _var_nicknames = dict()
+    _eval_vars = []
+    _special_vars = dict()
 
-    def __init__(self, **kw):
-        self.args = kw
+    def __init__(self, **statics):
+        self._static_vars.update(statics)
+        self._dynamic_var_names = dict()
+        for e in self._dynamic_vars:
+            if e in self._var_nicknames:
+                self._dynamic_var_names[e] = self._var_nicknames[e]
+            else:
+                self._dynamic_var_names[e] = e
         self.post_init()
 
     def post_init(self):
         pass
 
-    def f(self, x, **kwargs):
+    def execute(self, **dynamic):
+        for var in self._dynamic_vars:
+            if var not in dynamic:
+                raise ValueError(f"No such variable {var}")
+        return self._f(**dynamic, **self._static_vars)
+
+    def _f(self, x, **kw):
         return x
+
+    def compile(self, no_edit=False, include_static=True, **kwargs):
+        if not no_edit:
+            from re import sub
+            for e in kwargs:
+                if str(kwargs[e]).startswith('-'):
+                    kwargs[e] = f"({str(kwargs[e])})"
+        if include_static:
+            kwargs.update(self._static_vars)
+        result = self._str_repr.format(**kwargs)
+        if not no_edit:
+            result = sub(r"(\*1\b)|(\b1\*)", "", result)
+
+        return result
+
+    def get_unified(self):
+        return self.compile(**self._dynamic_var_names,
+                            **self._special_vars)
+
+    def get_local(self, **dynamic):
+        return self.compile(**dynamic,
+                            **self._special_vars) +\
+               f" = {self.execute(**dynamic)}"
 
     def __str__(self):
         return self._str_repr
 
 
 class MainExpr(Expression):
-    _str_repr = "{a}*{{x}}^3 + {b}*{a}*{{x}}^2 - {a}*{{x}} - {b}*{a} = {{ans}}"
+    _str_repr = "{a}*{x}^3 + {b}*{a}*{x}^2 - {a}*{x} - {b}*{a}"
 
-    def post_init(self):
-        self.a = Decimal(self.args['a'])
-        self.b = Decimal(self.args['b'])
-        self._str_repr = self._str_repr.format(a=self.args['a'],
-                                               b=self.args['b'],
-                                               x="x", ans="ans")
-
-    def f(self, x, **kwargs):
-        return self.a*x**3 + self.b*self.a*x**2 - self.a*x - self.b*self.a
+    def _f(self, x: Decimal, a: Decimal, b: Decimal, **kwargs):
+        return a*x**3 + b*a*x**2 - a*x - b*a
 
 
-class MainExprDerivative(MainExpr):
-    _str_repr = "3*{a}*{{x}}^2 + 2*{b}*{a}*{{x}} - {a} = {{ans}}"
+class MainExprDerivative(Expression):
+    _str_repr = "3*{a}*{x}^2 + 2*{b}*{a}*{x} - {a}"
 
-    def f(self, x, **kwargs):
-        return 3*self.a*x**2 + 2*self.b*self.a*x - self.a
+    def _f(self, x: Decimal, a: Decimal, b: Decimal, **kwargs):
+        return 3*a*x**2 + 2*b*a*x - a
 
 
 class SolveMethod:
     _method_name = "basic method"
-    _method_starts_repr = "x = g({x})"
-    _method_clear_repr = "x = g(x)"
-    _method_ends_repr = " = {x}\n"
     _check_repr = "\t(check f({x}+{epsilon})*f({x}-{epsilon}) < 0, {check_result})\n"
 
     def __init__(self, expr: Expression,
@@ -62,146 +92,230 @@ class SolveMethod:
         getcontext().prec = accuracy+2
         self.accuracy = accuracy
         self.epsilon = Decimal("1")/10**accuracy
+        self._stop_expr = self.StopExpression()
+        self._step_expr = self.StepExpression()
         self.post_init(**kwargs)
 
     def post_init(self, **kwargs):
         pass
 
+    def show_cache_prepare(self):
+        pass
+
     def step(self):
         pass
 
-    def show_repr(self, repr, **extra):
+    def show_step_prepare(self):
+        pass
+
+    def show_step(self, **kwargs):
         if self.suppres_info:
             return
-        print(repr.format(
-            x=self.x,
-            lbound=self.lbound,
-            rbound=self.rbound,
-            epsilon=self.epsilon,
-            **extra
-        ), end='')
+        print(f"x = {self._step_expr.get_unified()} = {self._step_expr.get_local(**kwargs)}")
 
-    def show_method_starts(self):
-        self.show_repr(self._method_starts_repr)
-
-    def show_method_ends(self):
-        self.show_repr(self._method_ends_repr)
-
-    def show_check(self):
-        self.show_repr(self._check_repr, check_result=self.stop_needed())
+    def show_check(self, result, **check_fargs):
+        if self.suppres_info:
+            return
+        print(f"\t\t{{ check {self._stop_expr.get_unified()} = {self._stop_expr.compile(**check_fargs)} = {result} }}\n")
 
     def stop_needed(self) -> bool:
-        return self.expr.f(self.x+self.epsilon) * \
-               self.expr.f(self.x-self.epsilon) < Decimal("0")
+        params = dict(
+            f_x1=self.expr.execute(x=self.x + self.epsilon),
+            f_x2=self.expr.execute(x=self.x - self.epsilon)
+        )
+        result = self._stop_expr.execute(**params) < Decimal("0")
+        self.show_check(result, **params)
+        return result
+
+    @staticmethod
+    def show_current_evaluation(expr: Expression, name: str = "f(x)", **kwargs):
+        print(f"\t{name} = {expr.get_local(**kwargs)}")\
+
+    @staticmethod
+    def show_first_evaluation(expr: Expression, name: str = "f(x)", **kwargs):
+        print(f"\t{name} = {expr.get_unified()} = {expr.get_local(**kwargs)}")
 
     def run(self):
         if not self.suppres_info:
             print(f"Attempt to find root in [{self.lbound}, {self.rbound}],\n"
                   f"Use '{self._method_name}':")
-
         i = 1
-        while not self.stop_needed():
+        if not self.suppres_info:
+            self.show_cache_prepare()
+        while i == 1 or not self.stop_needed():
             if not self.suppres_info:
-                print(f"Step #{i}:", self._method_clear_repr)
-            self.show_method_starts()
+                print(f"Step #{i}:")
+                self.show_step_prepare()
             self.step()
-            self.show_method_ends()
-            self.show_check()
             i += 1
         return self.x
+        
+    class StepExpression(Expression):
+        pass
 
-    def print_current_step(self):
-        print(str(self.expr).format(x=self.x,
-                                    ans=self.expr.f(self.x),
-                                    **self.extra))
+    class StopExpression(Expression):
+        _str_repr = "{f_x1}*{f_x2} < 0"
+        _dynamic_vars = ['f_x1', 'f_x2']
+        _static_vars = dict()
+        _var_nicknames = {
+            'f_x1': "f(x+e)",
+            'f_x2': "f(x-e)",
+        }
 
-
-class BinarySearchMethod(SolveMethod):
-    _method_name = "Метод половинных делений"
-    _method_starts_repr = "x = ({lbound}+{rbound})/2"
-    _method_clear_repr = "x = (a+b)/2"
-    _method_ends_repr = " = {x} => new bounds [{lbound}, {rbound}]\n"
-
-    def step(self):
-        self.x = (self.lbound+self.rbound)/2
-        if (self.expr.f(self.x) > 0) == (self.expr.f(self.lbound) > 0):
-            self.lbound = self.x
-        else:
-            self.rbound = self.x
+        def _f(self, f_x1, f_x2, **kwargs):
+            return f_x1 * f_x2
 
 
 class TangentMethod(SolveMethod):
     _method_name = "Метод касательных"
-    _method_starts_repr = "x = {x} - {f_x} / {df_x}"
-    _method_clear_repr = "x = x - f(x)/f'(x)"
-    _method_ends_repr = " = {x}\n"
 
     def post_init(self, **kwargs):
         self.x = self.rbound
         self.d_expr: Expression = kwargs['d_expr']
 
-    def show_method_starts(self):
-        self.show_repr(self._method_starts_repr,
-                       f_x=self.expr.f(self.x),
-                       df_x=self.d_expr.f(self.x),
-                       )
+    def show_step_prepare(self, **kwargs):
+        SolveMethod.show_current_evaluation(self.expr, x=self.x)
+        SolveMethod.show_current_evaluation(self.d_expr, x=self.x, name="f'(x)")
 
     def step(self):
-        self.x -= self.expr.f(self.x) / self.d_expr.f(self.x)
+        params = dict(
+            f_x=self.expr.execute(x=self.x),
+            df_x=self.d_expr.execute(x=self.x),
+            x=self.x
+        )
+        self.show_step(**params)
+        self.x = self._step_expr.execute(**params)
+
+    class StepExpression(Expression):
+        _str_repr = "{x} - {f_x}/{df_x}"
+        _dynamic_vars = ['x', 'f_x', 'df_x']
+        _static_vars = dict()
+        _var_nicknames = {
+            'df_x': "f'(x)",
+            'f_x': "f(x)",
+        }
+
+        def _f(self, df_x, f_x, x, **kwargs):
+            return x - f_x/df_x
 
 
 class SecantMethod(SolveMethod):
     _method_name = "Метод секущих"
-    _method_starts_repr = "x = {x} - {f_x}/({f_x} - {f_b})*({x}-{rbound})"
-    _method_clear_repr = "x = x - f(x)/(f(x)-f(b)) * (x-b)"
-    _method_ends_repr = " = {x}\n"
 
     def post_init(self, **kwargs):
         self.x = self.lbound
 
+    def show_cache_prepare(self):
+        print("Prepare:")
+        print("x = a =", self.lbound)
+        SolveMethod.show_current_evaluation(self.expr, x=self.rbound, name="f(b)")
+
     def show_method_starts(self):
         self.show_repr(self._method_starts_repr,
-                       f_x=self.expr.f(self.x),
-                       f_b=self.expr.f(self.rbound),
+                       f_x=self.expr.execute(x=self.x),
+                       f_b=self.expr.execute(x=self.rbound),
                        )
 
+    def show_step_prepare(self, **kwargs):
+        SolveMethod.show_current_evaluation(self.expr, x=self.x)
+
     def step(self):
-        self.x -= self.expr.f(self.x) /\
-                  (self.expr.f(self.x) - self.expr.f(self.rbound)) *\
-                  (self.x-self.rbound)
+        params = dict(
+            f_x=self.expr.execute(x=self.x),
+            f_b=self.expr.execute(x=self.rbound),
+            x=self.x,
+            b=self.rbound,
+        )
+        self.show_step(**params)
+        self.x = self._step_expr.execute(**params)
+
+    class StepExpression(Expression):
+        _str_repr = "{x} - ({f_x}*({x}-{b}))/({f_x}-{f_b})"
+        _dynamic_vars = ['x', 'f_x', 'f_b', 'b']
+        _static_vars = dict()
+        _var_nicknames = {
+            'f_x': "f(x)",
+            'f_b': "f(b)",
+        }
+
+        def _f(self, f_x, f_b, x, b, **kwargs):
+            return x - (f_x*(x-b))/(f_x-f_b)
 
 
 class SimpleIterationsMethod(SolveMethod):
     _method_name = "Метод простых итераций"
-    _method_starts_repr = "x = {x} - {k}*{f_x}"
-    _method_clear_repr = "x = x - k*f(x)"
-    _method_ends_repr = " = {x}\n"
 
     def post_init(self, **kwargs):
         self.x = (self.lbound+self.rbound)/Decimal("2")
-        df: Expression = kwargs['d_expr']
-        self.k = Decimal("1")/df.f(self.x)
+        self.d_expr: Expression = kwargs['d_expr']
+        self.k_expr = self.kExpr()
+        self.k = self.k_expr.execute(x=self.x, df_x=self.d_expr.execute(x=self.x))
 
-    def show_method_starts(self):
-        self.show_repr(self._method_starts_repr,
-                       f_x=self.expr.f(self.x),
-                       k=self.k,
-                       )
+    def show_cache_prepare(self):
+        print("Prepare:")
+        SolveMethod.show_first_evaluation(self.StartXExpr(), 'x',
+                                            a=self.lbound,
+                                            b=self.rbound)
+        SolveMethod.show_current_evaluation(self.d_expr, x=self.x, name="f'(x)")
+        SolveMethod.show_first_evaluation(self.k_expr, x=self.x, name="k",
+                                                df_x=self.d_expr.execute(x=self.x))
+
+    def show_step_prepare(self, **kwargs):
+        SolveMethod.show_current_evaluation(self.expr, x=self.x)
 
     def step(self):
-        self.x -= self.k*self.expr.f(self.x)
+        params = dict(
+            f_x=self.expr.execute(x=self.x),
+            x=self.x,
+            k=self.k,
+        )
+        self.show_step(**params)
+        self.x = self._step_expr.execute(**params)
+
+    class StartXExpr(Expression):
+        _str_repr = "({a}+{b})/2"
+        _dynamic_vars = ['a', 'b']
+        _static_vars = dict()
+
+        def _f(self, a, b, **kwargs):
+            return (a+b)/Decimal('2')
+
+    class StepExpression(Expression):
+        _str_repr = "{x} - {k}*{f_x}"
+        _dynamic_vars = ['x', 'f_x', 'k']
+        _static_vars = dict()
+        _var_nicknames = {
+            'f_x': "f(x)",
+        }
+
+        def _f(self, f_x, x, k, **kwargs):
+            return x - k*f_x
+
+    class kExpr(Expression):
+        _str_repr = "1/{df_x}"
+        _dynamic_vars = ['x', 'df_x']
+        _static_vars = dict()
+        _var_nicknames = {'df_x': "f'(x)"}
+
+        def _f(self, df_x, **kwargs):
+            return Decimal("1") / df_x
 
 
 def main():
-    a = "1"
-    b = "4.74"
-    extr1 = Decimal("-3.262")
-    extr2 = Decimal("0.102")
-    expr = MainExpr(a=a, b=b)
-    d_expr = MainExprDerivative(a=a, b=b)
+    a = input("Введите a: ")
+    b = input("Введите b: ")
+
+    expr = MainExpr(a=Decimal(a), b=Decimal(b))
+    d_expr = MainExprDerivative(a=Decimal(a), b=Decimal(b))
     b = Decimal(b)
+
+    print("f(x):", expr.get_unified())
+    print("f'(x):", d_expr.get_unified())
+
+    extr1 = Decimal(input("Введите extr1: "))
+    extr2 = Decimal(input("Введите extr2: "))
+    
     methods = [
-        #BinarySearchMethod,
         (TangentMethod, (extr2, Decimal("2"))),
         (SecantMethod, (-b+Decimal("0.5"), Decimal("-0.5"))),
         (SimpleIterationsMethod, (-b-Decimal("1"), extr1)),
@@ -216,7 +330,7 @@ def main():
                         accuracy=3,
                         )
         x = solver.run()
-        print(f"Root is {x}, proof: {expr.f(x)}")
+        print(f"Root is {x}, proof: {expr.execute(x=x)}")
 
 
 if __name__ == '__main__':
